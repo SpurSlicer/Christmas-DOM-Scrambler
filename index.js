1f870fa153bc3b7093bc9a536ee0b6da05a7e1d9
SpurSlicer <spur27@proton.me>
2024-12-25 12:56:06 -0700

    more small changes

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ const scramble = (element) => {
     if (element == undefined) return;
     element.style.width = `${Math.floor(Math.random()*window_width)+1}px`;
     element.style.height = `${Math.floor(Math.random()*window_height)+1}px`;
-    element.style.font = `${Math.floor(Math.random()*100)+1}px`;
+    element.style.fontSize = `${Math.floor(Math.random()*100)+1}px`;
     element.style.color = getChristmasColor();
     element.style.backgroundColor = getChristmasColor();
-    element.style.borderWidth = `${Math.floor(Math.random()*20)+1}px`;
+    element.style.borderWidth = `${Math.floor(Math.random()*10)+1}px`;
     element.style.borderStyle = `${((Math.floor(Math.random)*2) == 0) ? `solid` : `dashed`}`;
     element.style.borderColor = getChristmasColor();
     for (const child of element.children) {
         scramble(child);
     }
 }
-(() => { setInterval(() => {scramble(document.querySelector('body'));}, 100) })();
+(() => { document.querySelector('head title').textContent = `𝓜𝓮𝓻𝓻𝔂 𝓒𝓱𝓻𝓲𝓼𝓽𝓶𝓪𝓼!`; 
+         setInterval(() => {scramble(document.querySelector('body'));}, 100) })();
